7d3903c1ec013b6a409dc6a74cc0a951ec3ac860
crissHuang <735884048@qq.com>
2025-12-19 01:18:28 +0800

    【提交】提交element-plus内容

---
+++ element-plus-demo/src/App.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="el-row">
+    <el-button>default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello element plus"
+    }
+  },
+  methods: {
+    clickA:function () {
+      alert("hello wolrd")
+    }
+  }
+}
+</script>
