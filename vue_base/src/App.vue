a0865c1f00f0229c8f315742a768a677fc4acbcf
criss <735884048@qq.com>
2025-12-29 15:13:45 +0800

    【提交】v-if和v-show条件渲染

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import IfDemo from "@/components/IfDemo.vue";
 </script>
 
 <template>
   <HelloWorld />
+  <IfDemo />
 </template>
