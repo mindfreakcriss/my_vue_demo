a6f0884420797b780322f9f87b6b58460e5846e6
criss <735884048@qq.com>
2025-12-29 11:25:35 +0800

    【提交】提交模版语法

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -3,7 +3,11 @@
    data() {
       return {
         msg: 'Hello, Vue.js!',
-        hello:"是一个使用 Vue.js 框架编写的简单应用程序示例。"
+        hello:"是一个使用 Vue.js 框架编写的简单应用程序示例。",
+        number : 10, //求值
+        ok : true, //逻辑判断
+        message:"hello us", //数据运算
+        rawHtml:"<a href='https://www.baidu.com'>百度</a>" //原始html
       };
    }
  };
@@ -13,6 +17,11 @@
 
 <h3>墨宝语法</h3>
 <p>{{ msg }}</p>
-<p>{{hello}}</p>  
+<p>{{hello}}</p>
+<p>这是一个数字：{{ number + 1}}</p>
+<p> {{ok ? 'yes' : 'no'}} </p>
+<p>{{message.split("").reverse().join("")}}</p>
+  <p>{{rawHtml}}</p>
+  <p v-html="rawHtml"></p>
 
 </template>
