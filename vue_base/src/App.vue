8f9a454dc280c143fd69e0fe2507338641f67d4b
criss <735884048@qq.com>
2025-12-29 15:50:04 +0800

    【提交】事件处理

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -3,11 +3,15 @@ import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from "@/components/IfDemo.vue";
 import ListDemo from "@/components/ListDemo.vue";
 import KeyDemo from "@/components/KeyDemo.vue";
+import EventInlineDemo from "@/components/EventInlineDemo.vue";
+import EventMethodDemo from "@/components/EventMethodDemo.vue";
 </script>
 
 <template>
-  <HelloWorld />
-  <IfDemo />
-  <ListDemo></ListDemo>
-  <KeyDemo></KeyDemo>
+<!--  <HelloWorld />-->
+<!--  <IfDemo />-->
+<!--  <ListDemo></ListDemo>-->
+<!--  <KeyDemo></KeyDemo>-->
+  <EventInlineDemo></EventInlineDemo>
+  <EventMethodDemo></EventMethodDemo>
 </template>
