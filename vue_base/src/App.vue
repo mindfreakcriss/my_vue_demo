22cb7d4003270589817ebccf501cb8c2bd8a5954
criss <735884048@qq.com>
2025-12-29 11:14:45 +0800

    【提交】提交文本插值

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -2,7 +2,8 @@
  export default {
    data() {
       return {
-        msg: 'Hello, Vue.js!'
+        msg: 'Hello, Vue.js!',
+        hello:"是一个使用 Vue.js 框架编写的简单应用程序示例。"
       };
    }
  };
@@ -12,5 +13,6 @@
 
 <h3>墨宝语法</h3>
 <p>{{ msg }}</p>
+<p>{{hello}}</p>  
 
 </template>
