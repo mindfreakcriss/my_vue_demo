5dc883058405642564fd2186b3661a49eaa20b8d
criss <735884048@qq.com>
2025-12-29 11:13:00 +0800

    【提交】删除资源文件

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -1,8 +1,16 @@
-<script setup>
-
+<script>
+ export default {
+   data() {
+      return {
+        msg: 'Hello, Vue.js!'
+      };
+   }
+ };
 </script>
 
 <template>
 
+<h3>墨宝语法</h3>
+<p>{{ msg }}</p>
 
 </template>
