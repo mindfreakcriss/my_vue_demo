06cda92b490f5e3261ce5a246530ffccd72042d8
criss <735884048@qq.com>
2025-12-29 15:37:44 +0800

    【提交】key方便渲染

--- vue_base/src/App.vue
+++ vue_base/src/App.vue
@@ -2,10 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from "@/components/IfDemo.vue";
 import ListDemo from "@/components/ListDemo.vue";
+import KeyDemo from "@/components/KeyDemo.vue";
 </script>
 
 <template>
   <HelloWorld />
   <IfDemo />
   <ListDemo></ListDemo>
+  <KeyDemo></KeyDemo>
 </template>
---
+++ vue_base/src/components/KeyDemo.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+      data() {
+        return {
+          names:['John','Doe',"Joe"],
+        }
+      }
+  }
+</script>
+
+<template>
+<h3>Key属性添加到v-for</h3>
+  <p v-for="(item,index) in names" :key="index">{{item}}</p><!--推荐使用ID，不要用index , index 会变化-->
+</template>
+
+<style scoped>
+
+</style>
