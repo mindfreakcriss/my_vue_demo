e3866dba94f115dd24a81b2a38b0c29633ee7558
criss <735884048@qq.com>
2025-12-30 19:27:45 +0800

    【提交】组合式API-子传父数据

--- vue_base/src/composive/Child.vue
+++ vue_base/src/composive/Child.vue
@@ -2,6 +2,13 @@
 //数据接收
 import {ref} from "vue";
 
+//子传父数据
+const emit = defineEmits(['onSomeEvent'])
+const msg = ref("child数据")
+function sendMsg() {
+    emit("onSomeEvent", msg.value)
+}
+
 const props = defineProps({
   title : {
     type: String,
@@ -21,6 +28,7 @@ console.log(props)
   <h3>组合式API-Child</h3>
   {{ title }}
   {{ age }}
+  <button @click="sendMsg">发送数据</button>
 </template>
 
 <style scoped>
--- vue_base/src/composive/Parent.vue
+++ vue_base/src/composive/Parent.vue
@@ -2,12 +2,19 @@
   import Child from '@/composive/Child.vue'
   import { ref } from 'vue'
   const age = ref(20)
+
+  //获取子组件数据
+  const msg = ref("")
+  function getMsg(data) {
+    msg.value = data
+  }
 </script>
 
 <template>
 <h3>组合式API-Parent</h3>
    <!--数据传递-->
-  <child title="数据传递" :age="age"/>
+  <child title="数据传递" :age="age" @on-some-event="getMsg"/>
+  {{ msg }}
 </template>
 
 <style scoped>
