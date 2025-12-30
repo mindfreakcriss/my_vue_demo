acfa09822154c767d963654b8aa77da22442038a
criss <735884048@qq.com>
2025-12-30 10:25:05 +0800

    【提交】多种数据类型触传递

--- vue_base/src/components/Child.vue
+++ vue_base/src/components/Child.vue
@@ -9,7 +9,7 @@ export default{
 
   //props 数据传递，只能父级传递到子级，不能反其道
 
-  props: ['title','demo']
+  props: ['title','demo','age','names','userinfo'],
 }
 </script>
 
@@ -17,6 +17,11 @@ export default{
   <h3>child</h3>
   <p>{{ title }}</p>
   <p>{{ demo }}</p>
+  <p>{{ age }}</p>
+  <ul>
+    <li v-for="item in names">{{ item }}</li>
+  </ul>
+  <p>{{ JSON.stringify(userinfo) }}</p>
 </template>
 
 <style scoped>
