b44e889f32a7ceb1820e20cf13d61e4b470f5d28
criss <735884048@qq.com>
2025-12-31 10:09:27 +0800

    【提交】删除不必要内容

--- vue3-book-router/src/views/user/UserEdit.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>编辑用户</h1>
-    <p>用户编号 ： {{$route.params.id}}</p>
-      <!--query-->
-    <p>用户名称:{{$route.query.name}}</p>
-    <p>用户年龄：{{$route.query.age}}</p>
-  </div>
-</template>
--- vue3-book-router/src/views/user/UserStart.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>请选择用户查看和编辑</h1>
-  </div>
-</template>
